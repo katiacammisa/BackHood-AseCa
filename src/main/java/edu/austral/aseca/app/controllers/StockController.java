df881f0ce8845506d1d487409b34ace6b2162a6c
pedro <paraoz22@gmail.com>
2021-05-24 20:34:56 -0300

    buy-stock

--- src/main/java/edu/austral/aseca/app/controllers/StockController.java
+++ src/main/java/edu/austral/aseca/app/controllers/StockController.java
@@ -1,12 +1,38 @@
 package edu.austral.aseca.app.controllers;
 
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import edu.austral.aseca.app.models.Receipt;
+import edu.austral.aseca.app.services.StockService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.util.Optional;
 
 @CrossOrigin
 @RestController
 public class StockController {
-
+  
+  final StockService service;
+  
+  public StockController(StockService service) {
+    this.service = service;
+  }
+  
+  @GetMapping(path = "/{symbol}")
+  public String getCurrentPrice(@PathVariable String symbol) throws IOException, InterruptedException {
+    return service.getCurrentPrice(symbol);
+  }
+  
+  @PutMapping(path = "/{userId}/{symbol}/{quantity}")
+  public Receipt buy(@PathVariable Long userId, @PathVariable String symbol, @PathVariable Long quantity) {
+    try {
+      final Optional<Receipt> receipt = service.buyStock(symbol, quantity, userId);
+      if (receipt.isPresent()) return receipt.get();
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Not enough funds");
+    } catch (IOException | InterruptedException e) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
+  }
 }
--- src/main/java/edu/austral/aseca/app/services/StockService.java
+++ src/main/java/edu/austral/aseca/app/services/StockService.java
@@ -1,7 +1,47 @@
 package edu.austral.aseca.app.services;
 
+import edu.austral.aseca.app.models.Receipt;
+import edu.austral.aseca.app.models.User;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Optional;
 
 @Service
 public class StockService {
+
+  private final ApiService apiService;
+  private final UserService userService;
+  
+  public StockService(ApiService apiService, UserService userService) {
+    this.apiService = apiService;
+    this.userService = userService;
+  }
+  
+  public String getCurrentPrice(String symbol) throws IOException, InterruptedException {
+    return apiService.getCurrentPrice(symbol);
+  }
+  
+  public Optional<Receipt> buyStock(String symbol, double quantity, Long userId) throws IOException, InterruptedException {
+    final String currentPrice = getCurrentPrice(symbol);
+    final String gQ = getFromJson(currentPrice, "Global Quote");
+    final String stringPrice = getFromJson(gQ, "05. price");
+    final double price = Double.parseDouble(stringPrice);
+    final User user = userService.findById(userId);
+    if (user.getFunds() < price * quantity) return Optional.empty();
+    final Receipt r = new Receipt(userId, symbol, quantity, price);
+    return Optional.of(r);
+  }
+  
+  private static String getFromJson(String json, String property) {
+    try {
+      JSONObject fieldsJson = new JSONObject(json);
+      return fieldsJson.getString(property);
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
+    return "";
+  }
 }
