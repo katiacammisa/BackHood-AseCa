dbea3037de304180ea2828ae5d8cc5c56feb39bf
katiacammisa <43763141+katiacammisa@users.noreply.github.com>
2021-05-24 19:02:52 -0300

    Feature/stock models (#1)
    * Created all needed models
    
    * setup db and empty controller
    
    Co-authored-by: kuhamaven <khalil.stessens@ing.austral.edu.ar>
    Co-authored-by: pedro <paraoz22@gmail.com>

---
+++ src/main/java/edu/austral/aseca/app/models/TimeSeries.java
@@ -0,0 +1,12 @@
+package edu.austral.aseca.app.models;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+public class TimeSeries {
+  private final Map<LocalDateTime, StockValues> timeSeries = new HashMap<>();
+}
