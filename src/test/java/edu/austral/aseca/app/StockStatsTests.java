9cc85e04b91b394f584c81365923dffe4eac6c99
kuhamaven <khalil.stessens@ing.austral.edu.ar>
2021-06-15 23:15:28 -0300

    merged

--- src/test/java/edu/austral/aseca/app/StockStatsTests.java
+++ src/test/java/edu/austral/aseca/app/StockStatsTests.java
@@ -11,8 +11,6 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StockStatsTests {
 
